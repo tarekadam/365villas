ba79f82b382e5f7a3c473d602e6c86450fe4e126
Tarek Adam <tarek.adam@gmail.com>
2019-09-04 13:23:58 -0600

    adds bravo credentials to config

--- config/main.php
+++ config/main.php
@@ -1,2 +1,10 @@
 <?php
-return [];
+/**
+ * @see https://secure.365villas.com/vros/api/intro/
+ */
+return [
+	'base_url' => env('BASE_URL'),
+	'token'    => env('TOKEN'),
+	'key'      => env('KEY'),
+	'pass'     => env('PASS'),
+];
